b3af1f2eb40d73def7d6d68113c17871b4d76078
Ian Ramsey <ianrsk8s@gmail.com>
2021-11-23 07:00:50 -0600

    optimized and added UB

--- caam471/TSP.py
+++ caam471/TSP.py
@@ -7,6 +7,7 @@ import random
 
 def main():
     # Read in Raw, Finals, and Demand
+
     f = open("att48.txt")
     # f = open("berlin52.txt")
     # f = open("gr21.txt")
@@ -29,15 +30,32 @@ def TSP(f):
     #optimize model
     model.optimize()
 
+    #sorted weight dictionary
+    wDict = dict()
+    for i in range(numEdges):
+        wDict[edges[i][0],edges[i][1]] = weights[i]
+        wDict[edges[i][1],edges[i][0]] = weights[i]
+    wDict = dict(sorted(wDict.items(), key=lambda item: item[1]))
 
+    upper_bound = greedy_ub(wDict, numNodes, numEdges, edges, weights)
 
+    format_str = "{:<12s} {:<12s} {:<12s}"
+    print(format_str.format(*["Iteration","Lower Bound","Upper Bound"]))
+    print(format_str.format(*["0","0",str(upper_bound)]))
+
+    iter = 1
     while True:
         #if status comes back as optimal (value=2) then print out ony nonzero solution values
         if model.status != 2:
             break; #infeasible
 
         bestModel, bestValue = solve_relaxation(model, numNodes, numEdges, edges, weights)
-        print("Relaxation Solved! Current lower bound:" + str(bestValue))
+        # print("Relaxation Solved! Current lower bound: " + str(bestValue))
+        upper_bound = min([upper_bound, get_ub(bestModel, numNodes, numEdges, edges, weights, wDict)])
+        # print("Current upper bound: " + str(upper_bound))
+
+        print(format_str.format(*[str(iter),str(bestValue),str(upper_bound)]))
+        iter += 1
 
         if add_cut(model, bestModel, numNodes, numEdges, edges):
             break;
@@ -76,6 +94,33 @@ def read_file(f):
 
     return numNodes, numEdges, edges, weights
 
+
+def greedy_ub(wDict, numNodes, numEdges, edges, weights):
+    """
+    uses a greedy algorithm to solve TSP
+    """
+    #adjacency list
+    adj_list = [[] for i in range(numNodes)]
+    for edge in wDict.keys():
+        adj_list[edge[0]].append(edge[1])
+        adj_list[edge[1]].append(edge[0])
+
+    visited = set();
+    visited.add(0)
+
+    curr_node = 0
+    total = 0;
+    while len(visited) < numNodes:
+        for next_node in adj_list[curr_node]:
+            if not (next_node in visited):
+                break;
+            if next_node == adj_list[curr_node][-1]: #couldnt finish path
+                return np.inf
+        visited.add(next_node);
+        total += wDict[(curr_node,next_node)]
+        curr_node = next_node;
+
+    return total
 
 
 
@@ -125,6 +170,80 @@ def solve_relaxation(model, numNodes, numEdges, edges, weights):
 
     return bestModel, bestValue
 
+
+
+def get_ub(bestModel, numNodes, numEdges, edges, weights, wDict):
+    """
+    given a solution to the relaxation, create an UB by connecting the cycles
+    wDict is assumed to be sorted
+    """
+    bestX = bestModel.getVars();
+
+    #Use DFS to find connected components
+    V = range(numNodes)
+    E = defaultdict(list);
+    for i in range(numEdges):
+        if (bestX[i].x == 1):
+            E[edges[i][0]].append(edges[i][1])
+            E[edges[i][1]].append(edges[i][0])
+    is_reached, connectedness = is_connected(V,E,0)
+
+    cycles = [list(is_reached.keys()).copy()]
+    s = 1
+    while len(is_reached) < numNodes:
+        if not is_reached[s]:
+            new_reached, connectedness = is_connected(V,E,s)
+            cycles.append(list(new_reached.keys()).copy())
+            for v in new_reached.keys():
+                is_reached[v] = True
+        s += 1;
+
+    #rearrange data into useful format
+    max_edges = set()
+    free_nodes = set()
+    node2path = dict()
+    for cycle in cycles:
+        max_edge = (cycle[-1],cycle[0])
+        max_weight = wDict[max_edge]
+        for i in range(len(cycle) - 1):
+            node1 = cycle[i];
+            node2 = cycle[i+1];
+            edge = (node1,node2)
+            if wDict[edge] > max_weight:
+                max_edge = edge
+                max_weight = wDict[edge]
+
+        fcycle = np.flip(cycle).tolist()
+        free_nodes.add(max_edge[0])
+        node2path[max_edge[0]] = np.roll(fcycle, -fcycle.index(max_edge[0])).tolist()
+
+        free_nodes.add(max_edge[1])
+        node2path[max_edge[1]] = np.roll(cycle, -cycle.index(max_edge[1])).tolist()
+
+        max_edges.add((max_edge[0],max_edge[1]))
+        max_edges.add((max_edge[1],max_edge[0]))
+
+
+
+    #join cycles
+    eQueue = list(wDict.keys()).copy()
+    while len(free_nodes) > 2:
+        if len(eQueue) == 0: #can't join loops
+            return np.inf
+        e = eQueue.pop(0)
+        if (e[0] in free_nodes) and (e[1] in free_nodes) and not (e in max_edges):
+            free_nodes.remove(e[0])
+            free_nodes.remove(e[1])
+            new_start = node2path[e[0]][-1]
+            new_end = node2path[e[1]][-1]
+
+            node2path[new_start] = node2path[new_start] + node2path[e[1]]
+            node2path[new_end] = node2path[new_end] + node2path[e[0]]
+            max_edges.add((new_start,new_end))
+            max_edges.add((new_end,new_start))
+
+    path = node2path[free_nodes.pop()]
+    return wDict[(path[0],path[-1])] + sum(wDict[(path[i],path[i+1])] for i in range(numNodes - 1))
 
 
 def model_branch(origModel, varIdx, isLeft, bestModel, bestVal, numNodes, numEdges, edges, weights):
@@ -181,10 +300,25 @@ def add_cut(model, bestModel, numNodes, numEdges, edges):
     if connectedness:
         return True
 
-    #add cut
-    crossEdges = [x[i] for i in range(numEdges) if (is_reached[edges[i][0]] and not is_reached[edges[i][1]])
-                                                    or (is_reached[edges[i][1]] and not is_reached[edges[i][0]])]
-    model.addConstr(sum(crossEdges) >= 2)
+    cycles = [set(is_reached.keys()).copy()] ##### maybe remove if slow
+    s = 1
+    while len(is_reached) < numNodes:
+        if not is_reached[s]:
+            new_reached, connectedness = is_connected(V,E,s)
+            cycles.append(set(new_reached.keys()).copy())
+            for v in new_reached.keys():
+                is_reached[v] = True
+        s += 1;                                 ###### end maybe
+
+    # #add cut
+    # crossEdges = [x[i] for i in range(numEdges) if (is_reached[edges[i][0]] and not is_reached[edges[i][1]])
+    #                                                 or (is_reached[edges[i][1]] and not is_reached[edges[i][0]])]
+    # model.addConstr(sum(crossEdges) >= 2)
+
+    for cycle in cycles:
+        crossEdges = [x[i] for i in range(numEdges) if (((edges[i][0] in cycle) and not (edges[i][1] in cycle))
+                                                        or ((edges[i][1] in cycle) and not (edges[i][0] in cycle)))]
+        model.addConstr(sum(crossEdges) >= 2)
 
     #print("\t Size of loop conatining 0: " + str(sum(1 for i in is_reached.keys() if is_reached[i])))
 
